cecfa7e37ec44e4ebc42ce1f11679566af911d23
SuzanneRashka <suzanne.rashka@gmail.com>
2018-11-14 15:21:03 -0600

    attempt to fix missing api data in heroku

--- src/api/API.js
+++ src/api/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "http://www.omdbapi.com/?t=";
+const BASEURL = "https://www.omdbapi.com/?t=";
 const APIKEY = "&y=&plot=short&apikey=";
 export const KEY = process.env.REACT_APP_SECRET_KEY;
 
